bbb64b8f8fe489454001322a3e52ae99bae2d5bf
evanbern59 <evansantos1202@gmail.com>
2026-04-29 21:36:54 -0700

    added actor.cs

--- COMP003B.Assigment6/Models/Actor.cs
+++ COMP003B.Assigment6/Models/Actor.cs
@@ -1,6 +1,17 @@
-﻿namespace COMP003B.Assigment6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assigment6.Models
 {
     public class Actor
     {
+        public int ActorID { get; set; }
+
+        [Required]
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public virtual ICollection<MovieActor> MovieActors { get; set; }
     }
 }
