d2087a3f7a9ccc430c3e1748404ced2be81aeeb5
evanbern59 <evansantos1202@gmail.com>
2026-04-29 22:27:23 -0700

    added to program.cs

--- COMP003B.Assigment6/Program.cs
+++ COMP003B.Assigment6/Program.cs
@@ -1,3 +1,6 @@
+using COMP003B.Assigment6.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace COMP003B.Assigment6
 {
     public class Program
@@ -8,6 +11,9 @@ namespace COMP003B.Assigment6
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer("Name=ConnectionStrings: DefaultConnection"));
 
             var app = builder.Build();
 
