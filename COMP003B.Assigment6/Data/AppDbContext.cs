74b455ec6d1cb28b8b49209d3a8fb1d17a6a2f00
evanbern59 <evansantos1202@gmail.com>
2026-04-29 22:13:24 -0700

    added appdbcontext

--- COMP003B.Assigment6/Data/AppDbContext.cs
+++ COMP003B.Assigment6/Data/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace COMP003B.Assigment6.Data
+﻿using COMP003B.Assigment6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assigment6.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie>Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<MovieActor> MovieActors { get; set; }
     }
 }
